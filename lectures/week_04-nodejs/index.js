77441fbf46611c016c8d74a26470d0a5abf760e3
SOO OH <soooh@SOOs-MBP.fios-router.home>
2020-02-28 14:26:28 -0500

    p3

--- lectures/week_04-nodejs/index.js
+++ lectures/week_04-nodejs/index.js
@@ -1,37 +1,45 @@
-var fs = require('fs');
+var request = require('request');
 
-fs.writeFile('output.txt', 'The chicken crossed the road', function(err){
-	if (err) {
-		return console.error(err);
+request('http://api.umd.io/v0/courses/CMSC132', function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+		console.log(response);
 	}
-
-	fs.readFile('output.txt', function(err, data){
-		if (err) {
-			return console.error(err);
-		}
-		console.log("1. Inside: " + data);
-	})
-})
-
-
-fs.readFile('output.txt', function(err,data){
-	if(err) {
-		return console.error(err)
-	}
-
-	console.log("2. Outside: " + data)
-})
-/*
-fs.readFile('input.txt', function(err, data){
-	if (err) {
-		return console.error(err);
-	}
-	console.log("1. Asynchronous read: " + data);
 });
 
-var data = fs.readFileSync('input.txt');
-console.log("2. Sync read: " + data);
+// var fs = require('fs');
 
-console.log("3. Program done");
-*/
+// fs.writeFile('output.txt', 'The chicken crossed the road', function(err){
+// 	if (err) {
+// 		return console.error(err);
+// 	}
+
+// 	fs.readFile('output.txt', function(err, data){
+// 		if (err) {
+// 			return console.error(err);
+// 		}
+// 		console.log("1. Inside: " + data);
+// 	})
+// })
+
+
+// fs.readFile('output.txt', function(err,data){
+// 	if(err) {
+// 		return console.error(err)
+// 	}
+
+// 	console.log("2. Outside: " + data)
+// })
+
+// fs.readFile('input.txt', function(err, data){
+// 	if (err) {
+// 		return console.error(err);
+// 	}
+// 	console.log("1. Asynchronous read: " + data);
+// });
+
+// var data = fs.readFileSync('input.txt');
+// console.log("2. Sync read: " + data);
+
+// console.log("3. Program done");
+
 
